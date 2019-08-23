61dcec36c561081d58b84e2bab4cd9bbd1fa52ca
Mynd-Dev <mynd-dev@MyndComputer-iOS.local>
2019-08-23 11:24:46 +0800

    udpates

--- src/router.js
+++ src/router.js
@@ -1,7 +1,6 @@
 import Home from './views/Home.vue'
 import Router from 'vue-router'
 import Vue from 'vue'
-import ctc from './components/ContactUsBtn.vue'
 
 Vue.use(Router)
 
@@ -13,14 +12,6 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
   ]
 })
